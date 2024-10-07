a2aa9b181f076192a1a06ee8748a7863bb7126f1
3 <fatupatricia2004@gmail.com>
2024-10-07 23:43:15 +0300

    buttons with links

--- src/components/footer-comp.vue
+++ src/components/footer-comp.vue
@@ -1,49 +1,42 @@
 <template>
-  <v-footer class="footer">
+  <v-footer light class="footer">
     <v-row justify="center" no-gutters>
+
       <v-btn
           text
-          v-for="link in links"
-          :key="link"
-          color="white"
           x-large
-          outlined
           rounded
           class="ma-2"
-      >
-        {{ link }}
+      ><a href="https://www.instagram.com/best_clujnapoca/" target="_blank"><v-img src="../assets/igicon.png"></v-img></a>
       </v-btn>
+
+      <v-btn
+          text
+          x-large
+          rounded
+          class="ma-2"
+      ><a href="https://www.linkedin.com/company/best-cluj-napoca" target="_blank"><v-img src="../assets/linkedinicon.png"></v-img></a>
+      </v-btn>
+
+      <v-btn
+          text
+          x-large
+          rounded
+          class="ma-2"
+      ><a href="https://www.facebook.com/BESTcluj" target="_blank"><v-img src="../assets/fbicon.png"></v-img></a>
+      </v-btn>
+
       <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>CodeRun</strong>
       </v-col>
     </v-row>
+    
   </v-footer>
 </template>
 
-<script setup>
-const links = [
-  'linkedin',
-  'insta',
-  'mail',
-  'nrtel',
-  'utcn',
-  'best',
-]
-</script>
-
 <script>
 export default {
-  name: "footer-comp",
-  data: () => ({
-    links: [
-      'linkedin',
-      'insta',
-      'mail',
-      'nrtel',
-      'utcn',
-      'best',
-    ],
-  }),
+name: "footer-comp"
 }
 </script>
 
