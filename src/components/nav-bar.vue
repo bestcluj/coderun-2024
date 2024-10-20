bceef547737de8e8e0abb83881dc61e3914289c0
3 <fatupatricia2004@gmail.com>
2024-10-20 23:34:40 +0300

    added cr official logo to navbar and favicon

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app-bar class="navigation" flat color="#1b2653">
-    <v-toolbar-title>
+    <v-toolbar-title style="align-content: center;display:flex;">
       <router-link to="/" class="white--text links">
-        code.run
+        <img style="max-height:2rem;max-width:100%" src="../assets/cR.png">
       </router-link>
     </v-toolbar-title>
   </v-app-bar>
