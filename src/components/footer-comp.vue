db1f695afaa9dde4ffc76bab614e39374d875fc5
3 <fatupatricia2004@gmail.com>
2024-10-22 18:20:55 +0300

    Add contact info

--- src/components/footer-comp.vue
+++ src/components/footer-comp.vue
@@ -26,9 +26,19 @@
       ><a href="https://www.facebook.com/BESTcluj" target="_blank"><v-img src="../assets/icons8-facebook.svg"></v-img></a>
       </v-btn>
 
+      <v-col cols=12 class="text-center mt-4 contactinfo">
+        <div>
+          <a href="mailto:daria.lacatusu@bestcj.ro">daria.lacatusu@bestcj.ro</a>
+        </div>
+        <div>
+          <a href="https://api.whatsapp.com/send?phone=40755275307">+40 755 275 307</a>
+        </div>
+      </v-col>
+
       <v-col class="text-center mt-4" cols="12">
         {{ new Date().getFullYear() }} — <strong>CodeRun</strong>
       </v-col>
+
     </v-row>
 
   </v-footer>
@@ -44,5 +54,10 @@ name: "footer-comp"
 .footer{
   background-color: #e6acce;
   /*margin-bottom:0 !important;*/
+}
+
+
+.contactinfo a {
+  color: inherit;
 }
 </style>
