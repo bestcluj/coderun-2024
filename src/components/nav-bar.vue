f739ddf572fd368a7b25fbb33435572fd0984803
3 <fatupatricia2004@gmail.com>
2024-11-05 13:21:28 +0200

    basic menu

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -5,12 +5,43 @@
         <img style="max-height:2rem;max-width:100%" src="../assets/cR.png">
       </router-link>
     </v-toolbar-title>
+    <v-menu transition="slide-y-transition">
+      <template v-slot:activator="{ on }">
+        <v-btn icon="mdi mdi-dots-vertical" variant="text"  v-on="on" v-bind="props">
+<!--          <v-img src="../assets/menu-icon.svg" style="height:1em; width:1em"></v-img>-->
+          <v-icon style="color:#ffffff">mdi-menu</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, i) in items"
+                     :key="i"
+        >
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
   </v-app-bar>
 </template>
 
 <script>
+
+// const items = [
+//   {title: 'SCHEDULE'},
+//   {title: 'PARTNERS'},
+//   {title: '3,5%'},
+//   {title: 'ABOUT US'}
+//
+// ]
 export default {
-  name: "nav-bar"
+  name: "nav-bar",
+  data: () => ({
+    items: [
+      {title: 'SCHEDULE'},
+      {title: 'PARTNERS'},
+      {title: '3,5%'},
+      {title: 'ABOUT US'}
+    ]
+  })
 }
 </script>
 
