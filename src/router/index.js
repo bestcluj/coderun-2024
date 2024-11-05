66b08439087272f0190526b67998d9732a33e8f1
3 <fatupatricia2004@gmail.com>
2024-11-05 11:48:12 +0200

    week 1 and 2 schedule done

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import scheduleView from "@/views/schedule-view.vue";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/schedule',
+    name: 'schedule',
+    component: scheduleView
   },
   //{
     //path: '/about',
