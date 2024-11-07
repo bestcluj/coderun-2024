e4d00f65bd0cd896c16b9436d7352a6d90657d23
3 <fatupatricia2004@gmail.com>
2024-11-07 14:12:02 +0200

    meniu cu design

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -5,41 +5,48 @@
         <img style="max-height:2rem;max-width:100%" src="../assets/cR.png">
       </router-link>
     </v-toolbar-title>
-    <v-menu transition="slide-y-transition">
+
+<!--    meniu pentru ecranele mici-->
+
+    <v-menu transition="slide-y-transition" style="align-items: end; display: flex; justify-items: end">
       <template v-slot:activator="{ on }">
-        <v-btn icon="mdi mdi-dots-vertical" variant="text"  v-on="on" v-bind="props">
+        <v-btn class="responsive-menu" icon="mdi mdi-dots-vertical" variant="text"  v-on="on" v-bind="props">
 <!--          <v-img src="../assets/menu-icon.svg" style="height:1em; width:1em"></v-img>-->
           <v-icon style="color:#ffffff">mdi-menu</v-icon>
         </v-btn>
       </template>
-      <v-list>
+      <v-list class="menu">
         <v-list-item v-for="(item, i) in items"
                      :key="i"
         >
-            <v-list-item-title>{{item.title}}</v-list-item-title>
+            <v-list-item-title class="item-title"><router-link :to="item.route" style="text-decoration: none; color:white">{{item.title}}</router-link></v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
+
+<!--    meniu pentru ecranele mari-->
+
+    <v-row style="display:flex; align-content:end; justify-content:end;">
+      <v-col cols="12" class="responsive-row">
+        <router-link to="/schedule" class="white--text links ma-4">Schedule</router-link>
+        <router-link to="/partners" class="white--text links ma-4">Partners</router-link>
+        <a href="https://bestcj.ro/3-5-percent-program/" target="_blank" class="white--text links ma-4">3,5%</a>
+      </v-col>
+    </v-row>
+
+
   </v-app-bar>
 </template>
 
 <script>
-
-// const items = [
-//   {title: 'SCHEDULE'},
-//   {title: 'PARTNERS'},
-//   {title: '3,5%'},
-//   {title: 'ABOUT US'}
-//
-// ]
 export default {
   name: "nav-bar",
   data: () => ({
     items: [
-      {title: 'SCHEDULE'},
-      {title: 'PARTNERS'},
-      {title: '3,5%'},
-      {title: 'ABOUT US'}
+      {title: 'Schedule', route:'/schedule'},
+      {title: 'Partners', route:'/partners'},
+      {title: '3,5%', route: 'https://bestcj.ro/3-5-percent-program/'},
+      // {title: 'ABOUT US'}
     ]
   })
 }
@@ -49,13 +56,35 @@ export default {
 
 .links{
   color: #ffffff;
-  font-family: "radio stars";
+  font-family: "josefin sans";
   text-decoration: none;
+  font-size: large;
 }
 
 .navigation {
   position: sticky;
   display: flex; /* position:sticky automatically makes it inline-flex and breaks its size */
+}
+
+.menu{
+  background:#5b226a;
+  font-family:'josefin sans' !important;
+}
+
+@media only screen and (min-width:450px){
+  .responsive-menu{
+      display:none;
+  }
+}
+
+@media only screen and (max-width: 449px){
+  .responsive-row{
+    display:none;
+  }
+}
+
+.item-title{
+  color:#ffffff !important;
 }
 
 </style>
